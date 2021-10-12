cebace1a985f3dbfadf986036ae48bdbce42a3d1
Gleison Emanuel <gleison.spfc1208@hotmail.com>
2021-10-11 23:12:53 -0300

    adjust interfaces of email

--- src/Repository/EmailRepository.php
+++ src/Repository/EmailRepository.php
@@ -6,5 +6,6 @@ use App\Models\Email;
 
 interface EmailRepository
 {
-    public function add(Email $email): bool;
+    public function add(Email $email): Email;
+    public function update(Email $email): bool;
 }
